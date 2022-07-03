fe3b315da1bc11eb60bcde69183c82134dc97306
erlan <kalybekov.erlan@gmail.com>
2022-07-04 03:34:47 +0600

    module 4 task 5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYear;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("leap");
+        } else {
+            isLeapYear = false;
+            System.out.println("not leap");
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter d = new LeapYearPrinter();
+        d.isLeapYear(2012);
     }
 }
